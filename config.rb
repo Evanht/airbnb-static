357c446087008c8a90f393d11717f17e7a900ba0
Evan Thomas <evanhen@hotmail.co.uk>
2017-02-06 15:27:10 +0000

    finished home and team page

--- config.rb
+++ config.rb
@@ -31,7 +31,7 @@ activate :autoprefixer
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
 #  :which_fake_page => "Rendering a fake page with a local variable" }
 
-['evan', 'rob', 'bob', 'seb'].each do |name|
+['evan', 'robb', 'bob', 'seb'].each do |name|
   proxy "/flats/#{name}.html", "/flats/show.html", :locals => { :owner => name }, :ignore => true
 end
 
